5e4f98ffccaea7775a1cccab1eab718c5bc8b38f
Wan Nur Sofea <wannursofea@graduate.utm.my>
2025-05-26 11:24:59 +0800

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Kutip.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? ProfilePicture { get; set; }
+
+    }
+}
