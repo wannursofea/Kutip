60fa0dedaee62618bc4f59384db7ed494626618f
lowyingxi <lowxi@graduate.utm.my>
2025-05-29 21:55:46 +0800

    update

---
+++ Data/Migrations/20250529130935_newchanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kutip.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
